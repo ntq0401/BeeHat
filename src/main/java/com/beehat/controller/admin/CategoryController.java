08250d599d9658d403321fdd97421df5fe89a7f0
ntq0401 <ntq0401@gmail.com>
2024-12-15 22:06:22 +0700

    add modal product 2

--- src/main/java/com/beehat/controller/admin/CategoryController.java
+++ src/main/java/com/beehat/controller/admin/CategoryController.java
@@ -1,4 +1,84 @@
 package com.beehat.controller.admin;
 
+import com.beehat.entity.Belt;
+import com.beehat.entity.Category;
+import com.beehat.repository.BeltRepo;
+import com.beehat.repository.CategoryRepo;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/category")
 public class CategoryController {
+    @Autowired
+    BeltRepo beltRepo;
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+    @ModelAttribute("iconTitle")
+    String iconTitle() {
+        return "ph ph-list-dashes fs-3";
+    }
+    @ModelAttribute("pageTitle")
+    String pageTitle() {
+        return "Danh mục";
+    }
+    @ModelAttribute("listCategory")
+    List<Category> list() {
+        return categoryRepo.findAll();
+    }
+
+    @GetMapping("/index")
+    public String belt(Model model, @ModelAttribute("c") Category category) {
+        model.addAttribute("category", categoryRepo.findAll());
+        return "admin/category/category";
+    }
+
+    @PostMapping("/index")
+    public String create(@Valid @ModelAttribute("c") Category category, BindingResult rs, RedirectAttributes redirectAttributes) {
+        if (rs.hasErrors()) {
+            return "admin/category/category";
+        }
+        categoryRepo.save(category);
+        redirectAttributes.addFlashAttribute("message", "Thêm mới thành công!");
+        return "redirect:/admin/category/index";
+    }
+
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("c", categoryRepo.findById(id).orElse(null));
+        return "admin/category/form-category";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@Valid @ModelAttribute("c") Category category, BindingResult rs) {
+        if (rs.hasErrors()) {
+            return "admin/belt/form-belt";
+        }
+        categoryRepo.save(category);
+        return "redirect:/admin/category/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        Category category = categoryRepo.findById(id).orElse(null);
+        category.setStatus((byte) 0);
+        categoryRepo.save(category);
+        return "redirect:/admin/category/index";
+    }
+    @GetMapping("/on/{id}")
+    public String turnOn(@PathVariable("id") Integer id) {
+        Category category = categoryRepo.findById(id).orElse(null);
+        category.setStatus((byte) 1);
+        categoryRepo.save(category);
+        return "redirect:/admin/category/index";
+    }
 }
--- src/main/java/com/beehat/entity/Category.java
+++ src/main/java/com/beehat/entity/Category.java
@@ -1,6 +1,8 @@
 package com.beehat.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -12,15 +14,22 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
-    @Column(name = "name")
+    // Validate name to not be null, with length between 1 and 50 characters
+    @NotBlank(message = "Tên không được bỏ trống !")
+    @Size(max = 50, message = "Tên không được dài quá 50 kí tự !")
+    @Column(name = "name", nullable = false)
     private String name;
-    @Column(name = "status")
+
+    @Column(name = "status", nullable = false, insertable = false)
     private Byte status;
+
     @Column(name = "created_date", insertable = false, updatable = false)
     private LocalDateTime createdDate;
+
     @Column(name = "updated_date")
     private LocalDateTime updatedDate;
     @PreUpdate
